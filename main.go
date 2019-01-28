8dc82278e41a17f5fe23170030c08e45ff93eff7
hasnul <hasnul.rsusu@gmail.com>
2019-01-28 15:51:28 +0700

    progress

--- main.go
+++ main.go
@@ -3,13 +3,26 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"github.com/julienschmid"
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>Hello World</h1>")
+	
+	w.Header().Set("Content-Type", "text/html")
+	
+	if r.URL.Path == "/" {
+		fmt.Fprint(w, "<h1>Hello World Dynamic oke</h1>")	
+	} else if r.URL.Path == "/contact" {
+		fmt.Fprint(w, "<a href=\"mailto:arief.hasnul@gmail.com\">Contact us</a>")	
+	} else {
+		w.WriteHader(http.StatusNotFound)
+		fmt.Fprint(w, "<h1>404 Page not found</h1>")
+	}
 }
 
 func main() {
-	http.HandleFunc("/", handlerFunc)
+	mux := &http.ServeMux{}
+	mux.HandleFunc("/", handlerFunc)
+	//http.HandleFunc("/", handlerFunc)
 	http.ListenAndServe(":3000", nil)
 }
